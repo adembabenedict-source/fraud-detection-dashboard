36c7e3cc95cdc02d2c371e592656576ccb8f46d5
adembabenedict-source <adembabenedict@gmail.com>
2026-06-01 01:35:03 +0300

    Fix unterminated string on line 42

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ X = df[['Amount', 'Hour']]
 y = df['Fraud']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
 
-model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced
+model = RandomForestClassifier(n_estimators=100, random_state=42, class_weight='balanced')
